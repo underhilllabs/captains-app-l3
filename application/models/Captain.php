8e53e58f61df3eb5b710865e9e489370726e0c86
Bart Lantz <underhilllabs@gmail.com>
2013-04-21 20:41:04 -0400

    added Laravel model

--- application/models/Captain.php
+++ application/models/Captain.php
@@ -4,5 +4,5 @@ class Captain extends Eloquent {
     protected $guarded = array();
 
     public static $rules = array();
-    //public static $timestamps = true;
+    public static $timestamps = true;
 }
